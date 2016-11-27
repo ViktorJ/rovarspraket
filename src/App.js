640ba2e48cb3f79b785bbfd3e51fc469d53e961d
Viktor Jansson <viktorjansson@Viktors-MacBook-Air.local>
2016-11-27 23:55:41 +0100

    can now translate from both languageses, and some styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-cosmo.min.css';
 import './App.css';
-
 class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {translation: ""};
+    this.state = {
+      swedish: "",
+      robbers: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRobbers = this.handleRobbers.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleButton = this.handleButton.bind(this);
   }
 
   isConsonant(letter){
     return ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z"].indexOf(letter.toLowerCase()) !== -1
   }
 
+  handleChange(event){
+    this.setState({swedish: event.target.value});
+  }
+
+  handleRobbers(event){
+    this.setState({robbers: event.target.value});
+  }
+
   handleSubmit(event){
-    let text = this.refs.swedish.value;
-    this.translateToSwedish(text);
+    let text = "";
+
+    if (this.refs.swedish.value === ""){
+      text = this.state.robbers;
+      this.translateToSwedish(text);
+    } else {
+      text = this.state.swedish;
+      this.translateToRobbers(text);
+    }
+
+    event.preventDefault();
+  }
+
+  handleButton(event){
+    this.setState({
+      swedish: "",
+      robbers: ""
+    });
     event.preventDefault();
   }
 
@@ -31,7 +61,7 @@ class App extends Component {
         translatedString += text.charAt(i);
       }
       this.setState({
-        translation: translatedString
+        robbers: translatedString
       });
     }
   }
@@ -47,7 +77,7 @@ class App extends Component {
         translatedString += text.charAt(i);
       }
       this.setState({
-        translation: translatedString
+        swedish: translatedString
       });
     }
 
@@ -59,13 +89,18 @@ class App extends Component {
         <div>
           <h2>Rövarspråket!</h2>
         </div>
-        <form onSubmit={this.handleSubmit}>
+        <form className="form-inline" onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <input type="text" className="form-control" ref="swedish" placeholder="Svenska"></input>
+            <input type="text" className="form-control left" ref="swedish" placeholder="Svenska" value={this.state.swedish} onChange={this.handleChange}></input>
+          </div>
+          <div className="form-group">
             <button type="submit" className="btn btn-default">Översätt</button>
-            <input type="text" className="form-control" ref="robbers" value={this.state.translation} placeholder="Rövarspråk"></input>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control right" ref="robbers" placeholder="Rövarspråk" value={this.state.robbers} onChange={this.handleRobbers}></input>
           </div>
         </form>
+        <button type="button" className="btn btn-primary form-group" onClick={this.handleButton}>Rensa</button>
       </div>
     );
   }
